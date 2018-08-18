bd48161c9e01119a61bf113670dbacd41d6f9cc1
Drew Forbes <drewcforbes@gmail.com>
2018-08-18 17:45:05 -0500

    Add method to print combinations with higest sum of stats

--- print-start-options.py
+++ print-start-options.py
@@ -13,19 +13,51 @@ drawbridgeOptions = {
     'Brutalist': [1, 1, 1, 0, 0, 0]
 }
 
+moatOptions = {
+    'Sharks': [0, 0, 1, 1, 0, 0],
+    'Lava': [1, 0, 0, 0, 1, 0],
+    'Truth Serum': [0, 1, 0, 0, 0, 1]
+}
+
 murderHoleOptions = {
     'Gesture': [0, 0, 1, 0, 0, 0],
     'Cannon': [1, 0, 0, 0, 0, 0],
     'Catapult': [0, 1, 0, 0, 0, 0]
 }
 
+optionCombinations = {}
+
 for barbicanOption in barbicanOptions:
     for drawbridgeOption in drawbridgeOptions:
         for murderHoleOption in murderHoleOptions:
-            print(barbicanOption + ", " + drawbridgeOption + ", " + murderHoleOption)
+            for moatOption in moatOptions:
+                # Calculate summed stats for this combination
+                stats = [0, 0, 0, 0, 0, 0]
+                for x in range(6):
+                    stats[x] = barbicanOptions[barbicanOption][x] + drawbridgeOptions[drawbridgeOption][x] \
+                               + murderHoleOptions[murderHoleOption][x] + moatOptions[moatOption][x]
 
-            # Calculate and print sum of stats
-            sum = [0, 0, 0, 0, 0, 0]
-            for x in range(6):
-                sum[x] = barbicanOptions[barbicanOption][x] + drawbridgeOptions[drawbridgeOption][x] + murderHoleOptions[murderHoleOption][x]
-            print(sum)
+                optionCombinations[barbicanOption + ", " + drawbridgeOption + ", " + murderHoleOption + ", " + moatOption] = stats
+
+
+def print_all_combinations():
+    for optionCombination in optionCombinations:
+        print(optionCombination)
+        stats = optionCombinations[optionCombination]
+
+        statsSum = sum(stats)
+        print("Stats:" + str(stats) + " sum: " + str(statsSum))
+        print()
+
+# print_all_combinations()
+
+def print_combinations_with_highest_stats_sum():
+    for optionCombination in optionCombinations:
+        stats = optionCombinations[optionCombination]
+        statsSum = sum(stats)
+        if statsSum == 13:
+            print(optionCombination)
+            print("Stats:" + str(stats) + " sum: " + str(statsSum))
+            print()
+
+print_combinations_with_highest_stats_sum()
